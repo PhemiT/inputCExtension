86332d517ec950e35fc8abce206a360694bf8dee
Olubukunmi Olufemi <phemiwrites@gmail.com>
2022-02-19 14:34:27 +0100

    cleaned code

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 //declaring variables 
-let myLeads = ["", "", ""]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+
 
 inputBtn.addEventListener ("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    inputEl.value = ""
+    renderLeads()
 })
 
-for (i=0; i < myLeads.length; i++) {
-    console.log(myLeads[i])
+function renderLeads() { 
+    let listItems = ""
+    for (let i=0; i < myLeads.length; i++) {
+        listItems += '<li><a href="#">' + myLeads[i] + '</a></li>'
+    }
+    ulEl.innerHTML = listItems
 }
