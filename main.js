d9efd8894d6128e715f195dc81e64b256577f970
Olubukunmi Olufemi <phemiwrites@gmail.com>
2022-02-20 13:48:24 +0100

    manifest added

--- main.js
+++ main.js
@@ -15,7 +15,12 @@ inputBtn.addEventListener ("click", function() {
 function renderLeads() { 
     let listItems = ""
     for (let i=0; i < myLeads.length; i++) {
-        listItems += '<li><a href="#">' + myLeads[i] + '</a></li>'
+        listItems += `<li>
+        <a target= "_blank" href="${myLeads[i]}">
+            ${myLeads[i]}
+        </a>
+        </li>`
+        
     }
     ulEl.innerHTML = listItems
 }
